b3290db48bc8bb7cdce1f01ba81cb6a62b5641fb
vrajasekr <77961557+vrajasekr@users.noreply.github.com>
2021-01-27 11:29:56 +0530

    Add files via upload

---
+++ java/Activity1_1.java
@@ -0,0 +1,14 @@
+package sdet_java;
+public class Activity1_1 {
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2021;
+        toyota.color = "Silver";
+        toyota.transmission = "Manual";
+    
+        //Using Car class method
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+}
