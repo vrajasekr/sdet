0483028323c249a7a5e799e4d3d2fed74c5b17b3
vrajasekr <77961557+vrajasekr@users.noreply.github.com>
2021-01-28 11:32:07 +0530

    Add files via upload

---
+++ Java/Activity3_3a_Queue.java
@@ -0,0 +1,29 @@
+package sdet_java;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Activity3_3a_Queue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        //Add elements
+        for (int i=0;i<5;i++) {
+            q.add(i);
+        }
+
+        //Display contents of the queue. 
+        System.out.println("Elements in queue: " + q);
+
+        //To remove the head of queue.
+        int removeEle = q.remove();
+        System.out.println("removed element: " + removeEle);
+
+        //To view the head of queue
+        int headEle = q.peek();
+        System.out.println("head of queue: " + headEle);
+
+        int size = q.size();
+        System.out.println("Size of queue: " + size);
+    }
+}
